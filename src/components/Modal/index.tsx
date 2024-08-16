488c05b27497dc9b0ec1f2e09febff89490f9cab
Giovanni P. Oliveira <105515493+Giovanni1020@users.noreply.github.com>
2024-08-16 15:47:32 -0300

    feat: modal has now optional onClose prop

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,21 +1,27 @@
-import { FaXmark } from "react-icons/fa6";
-import StyledModal from "./style";
 import { ReactNode } from "react";
+import { FaXmark } from "react-icons/fa6";
 import { CustomState } from "../../utils/customState.hook";
+import StyledModal from "./style";
 
 type ModalProps = {
 	children?: ReactNode;
 	title: string;
 	modalState: CustomState<any>;
+	onClose?: () => void;
 };
 
-const Modal = ({ children, title, modalState }: ModalProps) => {
+const Modal = ({ children, title, modalState, onClose }: ModalProps) => {
 	return (
 		<StyledModal>
 			<div>
 				<header>
 					<h1> {title} </h1>
-					<button onClick={() => modalState.reset()}>
+					<button
+						onClick={() => {
+							modalState.reset();
+							onClose ? onClose() : null;
+						}}
+					>
 						<FaXmark />
 					</button>
 				</header>

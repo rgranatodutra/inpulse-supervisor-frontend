8e26d63193a5aed13eaaa9d80b4851e25b24eaa6
Giovanni P. Oliveira <105515493+Giovanni1020@users.noreply.github.com>
2024-08-19 17:50:00 -0300

    chore: mock route

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-const currentRoute = window.location.hostname.split('.')[0];
+const currentRoute = window.location.hostname.split(".")[0];
 
 class Services {
-    public static readonly users = axios.create({ baseURL: import.meta.env.VITE_USERS_URL + `/${currentRoute}` });
-    public static readonly customers = axios.create({ baseURL: import.meta.env.VITE_CUSTOMERS_URL + `/${currentRoute}` });
-    public static readonly exceptions = axios.create({ baseURL: import.meta.env.VITE_EXCEPTIONS_URL + `/${currentRoute}` });
-    public static readonly monitoring = axios.create({ baseURL: import.meta.env.VITE_MONITORING_URL + `/${currentRoute}` });
+	public static readonly users = axios.create({ baseURL: import.meta.env.VITE_USERS_URL + `/${currentRoute}` });
+	public static readonly customers = axios.create({ baseURL: import.meta.env.VITE_CUSTOMERS_URL + `/${currentRoute}` });
+	public static readonly exceptions = axios.create({
+		baseURL: import.meta.env.VITE_EXCEPTIONS_URL + `/${currentRoute}`,
+	});
+	public static readonly monitoring = axios.create({
+		baseURL: import.meta.env.VITE_MONITORING_URL + `/${currentRoute}`,
+	});
+	public static readonly mock = axios.create({ baseURL: import.meta.env.VITE_MOCK_URL });
 }
 
 export default Services;

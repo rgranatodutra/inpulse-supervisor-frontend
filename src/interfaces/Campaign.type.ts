72d722a3514c0aec36d85e65d928d2375ea59b9e
Giovanni P. Oliveira <105515493+Giovanni1020@users.noreply.github.com>
2024-08-20 17:38:52 -0300

    fix: forgot to commit campaigns type alongside the last feat  commit

---
+++ src/interfaces/Campaign.type.ts
@@ -0,0 +1,15 @@
+export type Campaign = {
+	CODIGO: number;
+	NOME: string;
+	DATA_INI?: string;
+	MAX_RESPOSTA?: number;
+	DESCRICAO?: string;
+	PRIORIDADE?: number;
+	SQL_CAMPANHA?: string;
+	GERADO_LISTA?: string;
+	PAUSADA?: string;
+	PAUSAR_AG_PUBLICA?: string;
+	TIPO?: string;
+	UNIDADE?: number;
+	OPERADOR?: number;
+};

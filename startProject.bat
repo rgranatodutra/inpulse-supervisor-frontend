98deacbc60157d921f4dfbfade6547aa3485b7f1
Giovanni P. Oliveira <105515493+Giovanni1020@users.noreply.github.com>
2024-09-02 15:21:52 -0300

    bonus: made a script to start the project

---
+++ startProject.bat
@@ -0,0 +1,10 @@
+start cmd.exe /k "npm start"
+cd ../campaigns-service
+start cmd.exe /k "npm run watch"
+cd ../inpulse-customers-service
+start cmd.exe /k "npm run watch"
+cd ../inpulse-instances-service
+start cmd.exe /k "npm run watch"
+cd ../inpulse-users-service
+start cmd.exe /k "npm run watch"
+
